f8da8c5daedd39415891742e3aa6801bc16321d4
nicholas-ooi <djfanaticx@gmail.com>
2017-03-28 00:40:04 +0800

    updated fixes, and changes for ui

--- smartbox/client/js/projects/projects.js
+++ smartbox/client/js/projects/projects.js
@@ -6,6 +6,7 @@ import { HTTP } from 'meteor/http';
 Template.projects.onCreated(function() {
 
   Session.setDefault("projects",null);
+  Session.setDefault("pastProjects",null);
   Session.setDefault("project",null);
   Session.setDefault("workers",null);
   Session.setDefault("statuses",null);
@@ -14,7 +15,12 @@ Template.projects.onCreated(function() {
   const user = Session.get("user");
   HTTP.call("GET", SERVER+"list_site_manager_projects", {params:{site_manager_id:user.user_id}},
   (error, result) => {
-      Session.set("projects", JSON.parse(result.content));
+    Session.set("projects", JSON.parse(result.content));
+  });
+
+  HTTP.call("GET", SERVER+"list_site_manager_projects", {params:{site_manager_id:user.user_id}},
+  (error, result) => {
+    Session.set("pastProjects", JSON.parse(result.content));
   });
 
 });
@@ -22,6 +28,9 @@ Template.projects.onCreated(function() {
 Template.projects.helpers({
   projects:() => {
     return Session.get("projects");
+  },
+  pastProjects:() => {
+    return _.filter(Session.get("pastProjects"), function(p){ return p.status=="completed"; });
   }
 });
 
